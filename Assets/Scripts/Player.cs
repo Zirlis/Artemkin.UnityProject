568b49cb0a3bfc0cbfc4f1ef974938ff7422ba2c
Zirlis <Zirlis@yandex.ru>
2020-12-28 23:44:26 +0700

    Final work

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,20 +6,23 @@ public class Player : MonoBehaviour
 
     [Header("Move")] 
 	[SerializeField] private float speed = 16f;
-	[SerializeField] private float rotSpeed = 0.75f;
+	[SerializeField] private float rotSpeed = 120f;
 	[SerializeField] private float modBackSpeed = 0.75f;
     [SerializeField] private float forceOfJump = 200f;
     [SerializeField] private float jumpCoolDown = 1f;
+    [SerializeField] private float eulerY;
+    [SerializeField] private float eulerX;
 
     [Header("Other")]
     [SerializeField] public int Key = 0;
+    [SerializeField] Transform Camera;
 
     private bool canJump = false;
     private float timeAfterJump = 0;
 
     private void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
@@ -34,14 +37,15 @@ public class Player : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody>().MoveRotation(GetComponent<Rigidbody>().rotation * Quaternion.Euler(Vector3.up * -rotSpeed));
+            GetComponent<Rigidbody>().AddForce(transform.right * -speed * modBackSpeed, ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody>().MoveRotation(GetComponent<Rigidbody>().rotation * Quaternion.Euler(Vector3.up * rotSpeed));
+            GetComponent<Rigidbody>().AddForce(transform.right * speed * modBackSpeed, ForceMode.Impulse);
         }
         
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+
+        if (Input.GetKey(KeyCode.Space) && canJump)
         {
             GetComponent<Rigidbody>().AddForce(Vector3.up * forceOfJump, ForceMode.Impulse);
             canJump = false;
@@ -51,6 +55,19 @@ public class Player : MonoBehaviour
 
     void Update()
 	{
+        float X = Input.GetAxis("Mouse X") * rotSpeed * Time.deltaTime;
+        float Y = -Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
+        eulerX = (Camera.rotation.eulerAngles.x + Y) % 360;
+        eulerY = (transform.rotation.eulerAngles.y + X) % 360;        
+
+        transform.rotation = Quaternion.Euler(0, eulerY, 0);
+        if (eulerX >= 60 && eulerX <= 90)
+            eulerX = 60;
+        if (eulerX <= 270 && eulerX >= 250)
+            eulerX = 270;
+
+        Camera.rotation = Quaternion.Euler(eulerX, eulerY, 0);
+
         if (timeAfterJump >= jumpCoolDown)
         {
             canJump = true;
